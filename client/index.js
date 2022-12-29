6f9cd8cf29f6cdf2870cee01c5c46b33cbfd68db
Davide Petti <petti.davide@gmail.com>
2022-12-29 16:27:33 +0100

    Add prover logic

--- client/index.js
+++ client/index.js
@@ -5,10 +5,14 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const nameIndex = 20;
+  const leaf = niceList[20];
+  const merkleTree = new MerkleTree(niceList);
+  const proof = merkleTree.getProof(nameIndex);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof,
+    leaf,
   });
 
   console.log({ gift });
--- utils/example.js
+++ utils/example.js
@@ -7,13 +7,14 @@ const merkleTree = new MerkleTree(niceList);
 
 // get the root
 const root = merkleTree.getRoot();
+console.log(`Merkle Root: ${root}`);
 
-// find the proof that norman block is in the list 
+// find the proof that norman block is in the list
 const name = 'Norman Block';
-const index = niceList.findIndex(n => n === name);
+const index = niceList.findIndex((n) => n === name);
 const proof = merkleTree.getProof(index);
 
 // verify proof against the Merkle Root
-console.log( verifyProof(proof, name, root) ); // true, Norman Block is in the list!
+console.log(verifyProof(proof, name, root)); // true, Norman Block is in the list!
 
 // TRY IT OUT: what happens if you try a name not in the list, or a fake proof?
